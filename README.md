3bf608c379c882c6040df9382bc54686559287c3
EazyReal <0312fs3@gmail.com>
2022-03-06 13:40:46 +0800

    add

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # CosmWasm NFTS
 
 ## Maxwill's Notes
-see `juno_client.toml.example`
+- main resource
+    - https://docs.junonetwork.io/smart-contracts-and-junod-development/
+    - https://docs.cosmwasm.com/docs/1.0/smart-contracts/contract-semantics
+- `juno_client.toml.example`
 - for adding key, change `$HOME/.juno/config/client.toml`
     - keyring-backend = "file"
+    - alternative:
+        - https://ahelpme.com/linux/dbusexception-could-not-get-owner-of-name-org-freedesktop-secrets-no-such-name/
+        - https://ahelpme.com/linux/dbusexception-could-not-get-owner-of-name-org-freedesktop-secrets-no-such-name/ 
 - for using test net, see [link](https://docs.junonetwork.io/smart-contracts-and-junod-development/junod-local-dev-setup#quickstart-on-the-testnet-with-a-public-node)
     - node="<public node RCP address>", e.g. "https://rpc.uni.junomint.com:443/"
     - chain-id = "uni-2"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,24 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 
 . $GIT_ROOT/.env
 
+# upload code
 TX=$(junod tx wasm store $GIT_ROOT/artifacts/cw721_fixed_price.wasm --from $KEY_NAME --chain-id=$CHAIN_ID --gas auto --gas-prices $GAS_PRICE --output json -y | jq -r '.txhash')
+# get code-id
 CODE_ID=$(junod query tx $TX --output json | jq -r '.logs[0].events[-1].attributes[0].value')
+
+# set init metadata
+INIT_META='{"name":"Poodle Coin","symbol":"POOD","decimals":6,"initial_balances":[{"address":"<validator-self-delegate-address>","amount":"12345678000"}]}'
+# instantiate contract code
+junod tx wasm instantiate $CODE_ID \
+    $INIT_META \
+    --amount 50000ujunox \
+    --label "Poodlecoin erc20" \
+    --from <your-key> --chain-id <chain-id> \
+    --gas auto \
+    -y
+
+# get contract address (this hold if the contract is the first initiated instance)
+CONTRACT_ADDR=$(junod query wasm list-contract-by-code $CODE_ID --output json | jq -r '.contracts[0]')
+
+# query contract address
+junod query wasm contract $CONTRACT_ADDR
